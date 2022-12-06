a439fd96a52c942528d5cc83d8a12883ad7f672b
alinpahont2912 <alin_pahontu@yahoo.com>
2022-12-06 12:10:35 +0200

    don't know anymore

--- src/pages/ProductsPage.vue
+++ src/pages/ProductsPage.vue
@@ -1,13 +1,8 @@
 <template>
   <div class="q-pa-md">
     <q-infinite-scroll @load="onLoad" :offset="250">
-<<<<<<< HEAD
-      <div class="row fit justify-evenly content-center">
-        <ProductTile v-for="item in items" :key="item.id" :product="item" />
-=======
       <div v-for="(item, index) in items" :key="index" class="caption">
         <ProductTile :product="item" />
->>>>>>> 8f744d416a1af96ea7354a81364e41654f7d358a
       </div>
       <template v-slot:loading>
         <div class="row justify-center q-my-md">
@@ -16,7 +11,6 @@
       </template>
     </q-infinite-scroll>
   </div>
-
 </template>
 <script setup>
 import { ref } from 'vue'
